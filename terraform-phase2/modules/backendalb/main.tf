65b403aef3f0d7426d4fddcd15513b0e962147c9
Irene <irene.engg2011@gmail.com>
2025-04-09 10:32:51 -0400

    Updated Terraform Phase 2

--- terraform-phase2/modules/backendalb/main.tf
+++ terraform-phase2/modules/backendalb/main.tf
@@ -2,13 +2,7 @@ data "aws_lb" "backend_alb" {
   name = "backend-alb" 
 }
 
-resource "aws_lb_listener" "backend" {
-  load_balancer_arn = data.aws_lb.backend_alb.arn  # Reference the existing ALB
+data "aws_lb_listener" "backend" {
+  load_balancer_arn = data.aws_lb.backend_alb.arn
   port              = 5000
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = var.target_group_backend_arn
-  }
 }
--- terraform-phase2/modules/frontendalb/main.tf
+++ terraform-phase2/modules/frontendalb/main.tf
@@ -2,13 +2,7 @@ data "aws_lb" "frontend_alb" {
   name = "frontend-alb" 
 }
 
-resource "aws_lb_listener" "frontend" {
-  load_balancer_arn = data.aws_lb.frontend_alb.arn  # Reference the existing ALB
+data "aws_lb_listener" "frontend" {
+  load_balancer_arn = data.aws_lb.frontend_alb.arn
   port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = var.target_group_frontend_arn
-  }
 }
